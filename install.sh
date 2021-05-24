1a1664fc2cc1f97dae9bd1998adf854f079ecd59
Matthew Isabel <matthewisabel@github.com>
2021-05-23 22:24:11 -0400

    Run test

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ cd ~/
 
 echo "" >> ~/.bashrc
 echo ". ~/dotfiles/bashprompt.sh" >> ~/.bashrc
+
+# Symlink dotfiles
+ln -s $(pwd)/bin "$HOME/bin"
+ln -s $(pwd)/gitconfig "$HOME/.gitconfig"
+ln -s $(pwd)/gitignore_global "$HOME/.gitignore_global"
